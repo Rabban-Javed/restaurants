395fe16245c99929ef5bac10b16f71be22a099d1
rabbanjaved786 <rjvloggermania@gmail.com>
2024-07-19 12:01:38 +0530

    initial commitss

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Rabban Javed</div>
+          <div className="right">
+            <p>M21/8 Batla house Okhla Delhi</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By R.J</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By R.J</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
